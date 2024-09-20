d91860ca3b14511d7527dfb2db0f7a58f02007cf
Laugh <70723697+xLaugh@users.noreply.github.com>
2024-09-20 16:05:40 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,8 +10,6 @@ files {
     "ui/assets/feu-position.svg",
     "ui/assets/feu-route.svg",
     "ui/assets/fuel.svg",
-    "ui/fonts/fonts/Roboto-Bold.ttf",
-    "ui/fonts/fonts/Roboto-Regular.ttf",
     "ui/script.js",
     "ui/style.css",
     "ui/debounce.min.js"
